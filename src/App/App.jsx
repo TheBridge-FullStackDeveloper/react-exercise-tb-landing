16d031fe31a3abd0a37d40f6dd38c750e5d5b7cd
Will-S <willschrick@gmail.com>
2025-02-01 04:51:01 -0800

    hecho 1-30 - reupload

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,19 @@
-import '../styles/global-fonts.css'                 /*'./styles/global.css'*/
-import '../components/Navbar'
-import Navbar from '../components/Navbar'
-import Header from '../components/Header'
-import Bootcamps from '../components/Bootcamps'
+import '../styles/global-fonts.css'; /*'./styles/global.css'*/
+import '../components/Navbar';
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
+import Bootcamps from '../components/Bootcamps';
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-    <Header/>
-    <Bootcamps/>
-
-  
+      <Navbar />
+      <Header />
+      <Bootcamps />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+//complete
